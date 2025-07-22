f6e04963820a21b38705efca3760ab89f9b5de4e
JeffersonCabello <jefcsanc@espol.edu.ec>
2025-07-22 10:42:30 -0500

    valor cada carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -48,5 +48,16 @@ const pedirCarta = () => {
 }
 
 //deck = [];
-pedirCarta();
-console.log(deck);
+//pedirCarta();
+
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor)) ? (valor === "A") ? 11 : 10
+        : valor * 1;
+}
+
+const valor = valorCarta("KD");
+
+console.log(valor);
+
+//console.log(deck);
